fa180a6001c56155f627d5c3bc28305d25d024fe
AJOsmaston <dajoxx@gmail.com>
2021-12-06 15:40:19 +0000

    jrwgjawrg

--- src/components/userManage/LogIn.js
+++ src/components/userManage/LogIn.js
@@ -62,7 +62,6 @@ const callAPI = (username, password) => {
   const user = { username: username, password: password };
   const options = {
     method: "post",
-    credentials: "include",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json",
