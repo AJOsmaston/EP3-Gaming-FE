07341a78153e1d553791eaaad7ab1d9b3f1dc2d0
AJOsmaston <dajoxx@gmail.com>
2021-12-06 10:52:07 +0000

    sessions hook

---
+++ src/hooks/useSessions.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export const useSessions = () => {
+  const [user, setUser] = useState();
+
+  const addUser = async () => {
+    
+  }
+
+  const deleteUser = async () => {
+
+  }
+
+}
